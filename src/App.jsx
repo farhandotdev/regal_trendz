1777b3694da60c84b3ae7c078509d18ac4d932fb
Mahendra Sen <mahendrasen249@gmail.com>
2023-10-17 10:31:46 +0530

    update app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+    <div>Hello I am Mahendra Owner of Regaltrends</div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
