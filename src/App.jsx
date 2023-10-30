c47904e8cb558800691b1fdab828035f241d30b0
mohammad shoaib <mohammadshoaib7221@gmail.com>
2023-10-31 00:36:14 +0530

    login page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-import Home from './Pages/Home/Home'
+import Login from './Pages/Login/Login'
 
 function App() {
   return (  
     <>
-      <Home/>
+      
+    
+      <Login/>
     </>
   )
 }
